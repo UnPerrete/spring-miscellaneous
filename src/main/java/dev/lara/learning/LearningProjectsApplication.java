1eeecc2f4056943610ee1566bf0629e93eda4d7b
UnPerrete <pelusapower52@gmail.com>
2025-04-21 10:44:05 +0100

    Initial commit

---
+++ src/main/java/dev/lara/learning/LearningProjectsApplication.java
@@ -0,0 +1,13 @@
+package dev.lara.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lara/learning/LearningProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lara.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
